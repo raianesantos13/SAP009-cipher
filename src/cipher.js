6057913d6a16f24dc46ecd7bbc0d2677f9c81c84
raianefsantos <raianefsantos13@gmail.com>
2023-02-09 17:52:32 -0300

    final

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,9 @@
 const cipher = {
 
   encode: function encode(key, encodeWord) {
-    console.log("encode")
-    if (encodeWord === undefined || typeof offset !== 'number' || encodeWord === 0 || encodeWord === null) {
+    if (!encodeWord || !key) {
       throw new TypeError('bad arguments');
     }
-  
     let encodedResult = "";
     let i;
     let encodeFormula;
@@ -16,23 +14,20 @@ const cipher = {
       if (indexLetter >= 65 && indexLetter <= 90) {
         encodeFormula = ((indexLetter - 65 + key) % 26) + 65;
         const newLetter = String.fromCharCode(encodeFormula)
-  
-  
+
         encodedResult += newLetter;
       } else if (indexLetter >= 97 && indexLetter <= 122) {
         encodeFormula = ((indexLetter - 97 + key) % 26) + 97;
         const newLetter = String.fromCharCode(encodeFormula);
         encodedResult += newLetter;
       } else {
-  
         encodedResult += encodeWord[i];
-  
       }
     }
     return encodedResult;
   },
   decode: function decode(key, decodeWord) {
-    if (decodeWord === undefined || typeof offset !== 'number' || decodeWord === 0 || decodeWord === null) {
+    if (!decodeWord || !key) {
       throw new TypeError('bad arguments');
     }
     let decodedResult = "";
@@ -41,7 +36,6 @@ const cipher = {
   
     for (i = 0; i < decodeWord.length; i++) {
       const indexLetter2 = decodeWord.charCodeAt(i);
-  
       if (indexLetter2 >= 65 && indexLetter2 <= 90) {
         decodeFormula = ((indexLetter2 - 90 - key) % 26 + 90);
         const newLetter2 = String.fromCharCode(decodeFormula);
@@ -57,6 +51,4 @@ const cipher = {
     return decodedResult;
   }
 };
-
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import cipher from "./cipher"
+import cipher from "./cipher.js"
 
-document.getElementById("encodebutton").addEventListener ("click", function() {
+document.getElementById("encodebutton").addEventListener("click", function () {
   const encodeWord = document.getElementById("encodetext").value;
   const key = parseInt(document.getElementById("cipherkey").value);
   document.getElementById("etext").innerHTML = cipher.encode(key, encodeWord);
 });
-
-document.getElementById("decodebutton").addEventListener ("click", function() {
+document.getElementById("decodebutton").addEventListener("click", function () {
   const decodeWord = document.getElementById("decodetext").value;
   const key = parseInt(document.getElementById("cipherkey").value);
-  
   document.getElementById("dtext").innerHTML = cipher.decode(key, decodeWord);
-    
+
 });
