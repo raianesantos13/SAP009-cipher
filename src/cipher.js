29c371ecfd21e2ae49deea385451786b315c3379
raianefsantos <raianefsantos13@gmail.com>
2023-02-08 21:30:12 -0300

    08-02

--- src/cipher.js
+++ src/cipher.js
@@ -1,57 +1,62 @@
-window.cipher = {
+const cipher = {
 
-  encode: encode,
-  decode: decode
+  encode: function encode(key, encodeWord) {
+    console.log("encode")
+    if (encodeWord === undefined || typeof offset !== 'number' || encodeWord === 0 || encodeWord === null) {
+      throw new TypeError('bad arguments');
+    }
+  
+    let encodedResult = "";
+    let i;
+    let encodeFormula;
+  
+    for (i = 0; i < encodeWord.length; i++) {
+      const indexLetter = encodeWord.charCodeAt(i);
+  
+      if (indexLetter >= 65 && indexLetter <= 90) {
+        encodeFormula = ((indexLetter - 65 + key) % 26) + 65;
+        const newLetter = String.fromCharCode(encodeFormula)
+  
+  
+        encodedResult += newLetter;
+      } else if (indexLetter >= 97 && indexLetter <= 122) {
+        encodeFormula = ((indexLetter - 97 + key) % 26) + 97;
+        const newLetter = String.fromCharCode(encodeFormula);
+        encodedResult += newLetter;
+      } else {
+  
+        encodedResult += encodeWord[i];
+  
+      }
+    }
+    return encodedResult;
+  },
+  decode: function decode(key, decodeWord) {
+    if (decodeWord === undefined || typeof offset !== 'number' || decodeWord === 0 || decodeWord === null) {
+      throw new TypeError('bad arguments');
+    }
+    let decodedResult = "";
+    let i;
+    let decodeFormula;
+  
+    for (i = 0; i < decodeWord.length; i++) {
+      const indexLetter2 = decodeWord.charCodeAt(i);
+  
+      if (indexLetter2 >= 65 && indexLetter2 <= 90) {
+        decodeFormula = ((indexLetter2 - 90 - key) % 26 + 90);
+        const newLetter2 = String.fromCharCode(decodeFormula);
+        decodedResult += newLetter2;
+      } else if (indexLetter2 >= 97 && indexLetter2 <= 122) {
+        decodeFormula = ((indexLetter2 - 122 - key) % 26 + 122);
+        const newLetter2 = String.fromCharCode(decodeFormula);
+        decodedResult += newLetter2;
+      } else {
+        decodedResult += decodeWord[i];
+      }
+    }
+    return decodedResult;
+  }
 };
 
-function encode(key, encodeWord) {
 
-  let encodedResult = "";
-  let i;
-  let encodeFormula;
-
-  for (i = 0; i < encodeWord.length; i++) {
-    let indexLetter = encodeWord.charCodeAt(i);
-
-    if (indexLetter >= 65 && indexLetter <= 90) {
-      encodeFormula = ((indexLetter - 65 + key) % 26) + 65;
-      let newLetter = String.fromCharCode(encodeFormula)
-
-
-      encodedResult += newLetter;
-    } else if (indexLetter >= 97 && indexLetter <= 122) {
-      encodeFormula = ((indexLetter - 97 + key) % 26) + 97;
-      let newLetter = String.fromCharCode(encodeFormula);
-      encodedResult += newLetter;
-    } else {
-
-      encodedResult += encodeWord[i];
-
-    }
-  }
-  return encodedResult;
-}
-
-function decode(key, decodeWord) {
-  let decodedResult = "";
-  let i;
-  let decodeFormula;
-
-  for (i = 0; i < decodeWord.length; i++) {
-    let indexLetter2 = decodeWord.charCodeAt(i);
-
-    if (indexLetter2 >= 65 && indexLetter2 <= 90) {
-      decodeFormula = ((indexLetter2 - 90 - key) % 26 + 90);
-      let newLetter2 = String.fromCharCode(decodeFormula);
-      decodedResult += newLetter2;
-    } else if (indexLetter2 >= 97 && indexLetter2 <= 122) {
-      decodeFormula = ((indexLetter2 - 122 - key) % 26 + 122);
-      let newLetter2 = String.fromCharCode(decodeFormula);
-      decodedResult += newLetter2;
-    } else {
-      decodedResult += decodeWord[i];
-    }
-  }
-  return decodedResult;
-}
-
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+import cipher from "./cipher"
 
-
-document.getElementById("encodebutton").addEventListener ("click", function encodeBtnFunction() {
-  let encodeWord = document.getElementById("encodetext").value;
-  let key = parseInt(document.getElementById("cipherkey").value);
-  document.getElementById("etext").innerHTML = window.cipher.encode(key, encodeWord);
+document.getElementById("encodebutton").addEventListener ("click", function() {
+  const encodeWord = document.getElementById("encodetext").value;
+  const key = parseInt(document.getElementById("cipherkey").value);
+  document.getElementById("etext").innerHTML = cipher.encode(key, encodeWord);
 });
 
-document.getElementById("decodebutton").addEventListener ("click", function decodeBtnFunction() {
-  let decodeWord = document.getElementById("decodetext").value;
-  let key = parseInt(document.getElementById("cipherkey").value);
+document.getElementById("decodebutton").addEventListener ("click", function() {
+  const decodeWord = document.getElementById("decodetext").value;
+  const key = parseInt(document.getElementById("cipherkey").value);
   
-  document.getElementById("dtext").innerHTML = window.cipher.decode(key, decodeWord);
+  document.getElementById("dtext").innerHTML = cipher.decode(key, decodeWord);
     
 });
